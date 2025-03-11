8196f47b9284cf1d83204310dfe04ca6dbf4bdea
Tokam Lilian <tokamlilian@gmail.com>
2025-03-11 11:39:12 -0400

    deleted directories

--- Creations/Tables/src_fixtures.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [src].[src_fixtures]
-(
-	[fixtures] VARCHAR (MAX)
-	,[load_date] DATETIME2
-)
--- Creations/Tables/src_team_rankings.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [src].[src_team_rankings]
-(
-	[rankings] VARCHAR (200)
-	,[load_date] DATETIME2
-)
